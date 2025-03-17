1512c0cf179ab45167ae4f6fb6a86ef1e9de9859
erive92d <erive92d@gmail.com>
2025-03-16 18:14:09 -0700

    first

---
+++ app/components/homecomps/HeroComp.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+
+
+export default function HeroComp() {
+    return (
+        <section className="relative flex items-center justify-center h-screen text-white">
+            {/* Background Image */}
+            <div className="absolute inset-0 -z-10">
+                <Image
+                    src={"/herobg.jpg"}
+                    alt="Hero Background"
+                    layout="fill"
+                    objectFit="cover"
+                    className="object-bottom"
+                    quality={80}
+                />
+                <div className="absolute inset-0 bg-black/50"></div>            </div>
+
+            {/* Content */}
+            <div className="text-center px-6">
+                <h1 className="text-4xl md:text-6xl font-bold">Welcome to Ricethenics Website</h1>
+                <p className="text-lg md:text-xl mt-4">
+                    We love to run and eat rice.
+                </p>
+                <button className="btn btn-ghost bg-lime-400 text-black mt-2">
+                    View runs
+                </button>
+            </div>
+        </section>
+    );
+
+}
---
+++ app/components/navcomps/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className="navbar bg-black py-8 px-12 border-b border-lime-300">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu text-lg menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a className=''>Runs</a></li>
+                        <li>
+                            <a href="
+                            ">
+                                Calendar
+                            </a>
+                        </li>
+                        <li><a>About</a></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-2xl text-lime-400 italic font-bold">RICETHENICS</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-lg">
+                    <li><a className=''>Runs</a></li>
+                    <li>
+                        <a href="
+                            ">
+                            Calendar
+                        </a>
+                    </li>
+                    <li><a>About</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn btn-ghost bg-lime-400 text-black">Join us</a>
+            </div>
+        </div>
+    )
+}
